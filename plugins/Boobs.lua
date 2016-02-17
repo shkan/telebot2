507409693b33e94d26b6520885a0326e70ad1ec7
Shayan123456 <shayan_jon123@yahoo.com>
2016-02-17 19:59:10 +0330

    Update Boobs.lua

--- plugins/Boobs.lua
+++ plugins/Boobs.lua
@@ -37,15 +37,38 @@ local function getRandomBoobs(attempt)
   return 'http://media.oboobs.ru/' .. data.preview
 end
 
+local function getRandomfilms(attempt)
+  attempt = attempt or 0
+  attempt = attempt + 1
+
+  local res,status = http.request("http://shayanpanapa.ir/1.html")
+
+  if status ~= 200 then return nil end
+  local data = json:decode(res)[1]
+
+  -- The OpenBoobs API sometimes returns an empty array
+  if not data and attempt <= 3 then
+    print('Cannot get that butts, trying another one...')
+    return getRandomfilms(attempt)
+  end
+
+  return 'http://shayanpanapa.ir/' .. data.preview
+end
+
+
 local function run(msg, matches)
   local url = nil
   
-  if matches[1] == "!boobs" then
+  if matches[1] == "!ممه" then
     url = getRandomBoobs()
   end
 
-  if matches[1] == "!butts" then
+  if matches[1] == "!کون" then
     url = getRandomButts()
+  end
+  
+    if matches[1] == "!فیلم" then
+    url = getRandomfilms()
   end
 
   if url ~= nil then
@@ -59,12 +82,14 @@ end
 return {
   description = "Gets a random boobs or butts pic", 
   usage = {
-    "!boobs: Get a boobs NSFW image. ًں”‍",
-    "!butts: Get a butts NSFW image. ًں”‍"
+    "!ممه: Get a boobs NSFW image. ًں”‍",
+    "!کون: Get a butts NSFW image. ًں”‍",
+    "!فیلم: Get a butts NSFW image. ًں”‍"
   },
   patterns = {
-    "^!boobs$",
-    "^!butts$"
+    "^!ممه$",
+    "^!کون$",
+    "^!فیلم$"
   }, 
   run = run 
 }

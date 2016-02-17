74f5babe88851abac26c285b2455692f2dd6ea7e
Shayan123456 <shayan_jon123@yahoo.com>
2016-02-17 19:58:16 +0330

    Create version.lua

---
+++ plugins/version.lua
@@ -0,0 +1,26 @@
+do
+
+function run(msg, matches)
+  return 'S.H.O.S Telegram Bot v1.5'.. [[ 
+  
+  Website:
+  http://shayanpanapa.IR
+  Antispam Bot: @Anti_Spam_Shayan123_Bot
+  Sudo (Admin): @shayan123hacker
+  
+  Powered by:
+  shayan soft Co. Group
+  Engineer Shayan H.j
+  http://shayan-soft.IR]]
+end
+
+return {
+  description = "Robot and Creator About", 
+  usage = "/ver : robot info",
+  patterns = {
+    "^[!/]ver$"
+  }, 
+  run = run 
+}
+
+end
